fe1127a14f164aa51e5417f67ab6e6fe5c78c62b
Kevin Lee <pwntastickevin@gmail.com>
2017-10-18 16:31:25 -0600

    clock works!

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,7 +1,37 @@
 $(document).ready(function() {
   $('.shirt-swap-rt').on('click', function() {
-    $('.right-image').append($('.shirt-swap-rb'))
+    $('.shirt-swap-rt').removeClass()
+    $('.right-image').removeClass()
+    $('.shirt-swap-rt').addClass($('right-image'))
+    // $('.shirt-swap-rt').addClass('right-image')
 
     console.log('working?')
   })
+
+  var countDownDate = new Date("Oct 18, 2018 14:57:25").getTime();
+
+  var x = setInterval(function() {
+
+    var now = new Date().getTime();
+
+
+    var distance = countDownDate - now;
+
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("demo").innerHTML =  hours + ":"
+    + minutes + ":" + seconds + "s";
+
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
 });
