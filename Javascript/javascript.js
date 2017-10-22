9a368e4bdb8526c02f93c85066d01ac604e379af
Kevin Lee <pwntastickevin@hotmail.com>
2017-10-21 22:49:47 -0600

    updates...?

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -9,15 +9,9 @@ $(document).ready(function() {
   })
 
   var countDownDate = new Date("Oct 18, 2018 14:57:25").getTime();
-
   var x = setInterval(function() {
-
     var now = new Date().getTime();
-
-
     var distance = countDownDate - now;
-
-
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -26,8 +20,7 @@ $(document).ready(function() {
 
     document.getElementById("demo").innerHTML =  hours + ":"
     + minutes + ":" + seconds + "s";
-
-
+    
     if (distance < 0) {
       clearInterval(x);
       document.getElementById("demo").innerHTML = "EXPIRED";
